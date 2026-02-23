0e93d1657aa57834d4a6046dea245237fc63b678
Dmitrii <sorokindm1try@yandex.ru>
2026-02-23 10:49:28 +0300

    homework 1.3

--- src/main/java/com/example/car/controller/CarController.java
+++ src/main/java/com/example/car/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -21,82 +22,75 @@ public class CarController {
     CarRepository carRepository;
 
     @GetMapping("/hi")
-    public String test(Model model,String sortBy){
-        model.addAttribute("cars",carRepository.findAll());
+    public String test(Model model, String sortBy) {
+        model.addAttribute("cars", carRepository.findAll());
         return "pages/car/index";
     }
 
     @PostMapping("/create")
-    public String create(Model model,Car car){
+    public String create(Model model, Car car) {
         carRepository.save(car);
-        return "pages/car/index";
+        return "redirect:/car/hi";
     }
 
     @GetMapping("/addAll")
-    public String addAll(Model model){
-        boolean isAdd=false;
-        if (carRepository.count()==0){
+    public String addAll(RedirectAttributes redirectAttributes) {
+        boolean isAdd = false;
+        if (carRepository != null && carRepository.count() == 0) {
             add_10_cars();
-            isAdd=true;
+            isAdd = true;
         }
-        model.addAttribute("status",isAdd);
-       return "pages/car/index";
+//        model.addAttribute("status",isAdd);
+        redirectAttributes.addFlashAttribute("status", isAdd);
+        return "redirect:/car/hi";
     }
 
     @GetMapping("/deleteAll")
-    public String delete(Model model){
+    public String delete(Model model) {
         carRepository.deleteAll();
         return "pages/car/index";
     }
 
     @GetMapping("/add")
-    public String add(Model model){
-        model.addAttribute("title","Create page");
-        model.addAttribute("car",new Car());
+    public String add(Model model) {
+        model.addAttribute("title", "Create page");
+        model.addAttribute("car", new Car());
         return "pages/car/create";
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteById(@PathVariable Long id,Model model){
+    public String deleteById(@PathVariable Long id, Model model) {
         carRepository.deleteById(id);
-        return "pages/car/index";
+        return "redirect:/car/hi";
 
     }
 
     @GetMapping("/edit/{id}")
-    public String updateById(@PathVariable Long id,Model model){
-        Optional<Car> findCar=carRepository.findById(id);
-        model.addAttribute("title","Update Page");
-        model.addAttribute("car",findCar.get());
+    public String updateById(@PathVariable Long id, Model model) {
+        Optional<Car> findCar = carRepository.findById(id);
+        model.addAttribute("title", "Update Page");
+        model.addAttribute("car", findCar.get());
         return "pages/car/edit";
     }
 
 
-
-
-    public void add_10_cars(){
+    public void add_10_cars() {
 
         for (int i = 0; i < 10; i++) {
-        Car car=new Car();
+            Car car = new Car();
 
-        String brand=faker.vehicle().make();
-        String model=faker.vehicle().model();
-        Integer year=faker.number().numberBetween(2000,2025);
-        String color= faker.color().name();
+            String brand = faker.vehicle().make();
+            String model = faker.vehicle().model();
+            Integer year = faker.number().numberBetween(2000, 2025);
+            String color = faker.color().name();
 
-        car.setBrand(brand);
-        car.setModel(model);
-        car.setYear(year);
-        car.setColor(color);
+            car.setBrand(brand);
+            car.setModel(model);
+            car.setYear(year);
+            car.setColor(color);
 
-        carRepository.save(car);
+            carRepository.save(car);
         }
-
-
-
-
-
-
 
 
     }
